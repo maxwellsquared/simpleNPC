0e7cb97f931833f7fb9061cd382877c223618a8d
Max K <maxkuhn@gmail.com>
2022-11-04 16:35:57 -0700

    Remove Firebase

--- src/App.js
+++ src/App.js
@@ -8,113 +8,20 @@ import { Portrait } from './components/Portrait';
 import { wordPicker } from './helpers/wordPicker';
 import { maleNames, femaleNames, nbNames, lastNames, desc1, species, charJob } from "./helpers/arrays";
 
-// FIREBASE STUFF
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import 'firebase/compat/auth';
-import 'firebase/compat/analytics';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { useCollectionData } from 'react-firebase-hooks/firestore';
-import { FacebookAuthProvider } from 'firebase/auth';
-
-firebase.initializeApp({
-  apiKey: "AIzaSyDlyuCM5wTtP2_5PPSCbluqM119CjIpUJg",
-  authDomain: "simplenpc-7d5f4.firebaseapp.com",
-  projectId: "simplenpc-7d5f4",
-  storageBucket: "simplenpc-7d5f4.appspot.com",
-  messagingSenderId: "1061276116146",
-  appId: "1:1061276116146:web:4ee84a2eab6d5cd76a15ab",
-  measurementId: "G-57YR4FLDNZ"
-})
-
-const auth = firebase.auth();
-const firestore = firebase.firestore();
-
 function App() {
-  const [user] = useAuthState(auth);
 
-  
   return (
     <div className="App">
       <header>
         <h1>simpleNPC</h1>
       </header>
       <section>
-        <Portrait />
+        {/* <Portrait /> */}
         <p>{wordPicker(maleNames, 1)} {wordPicker(lastNames, 1)} is a {wordPicker(desc1, 1)} {wordPicker(species, 1)} {wordPicker(charJob, 1)}.</p>
         <StatBlock />
         </section>
     </div>
   );
-}
-function SignIn() {
-  const signInWithGoogle = () => {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    auth.signInWithPopup(provider);
-  }
-
-  return (
-    <button className="sign-in" onClick={signInWithGoogle}>SIGN IN</button>
-  )
-}
-
-function SignOut() {
-  return auth.currentUser && (
-    <button className="sign-out" onClick={() => auth.signOut()}>SIGN OUT</button>
-  )
-}
-
-function ChatRoom() {
-
-  const dummy = useRef();
-  const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
-
-  const [messages] = useCollectionData(query, { idField: 'id' });
-  const [formValue, setFormValue] = useState('');
-
-  const sendMessage = async (event) => {
-    event.preventDefault();
-    const { uid } = auth.currentUser;
-
-    await messagesRef.add({
-      text: formValue,
-      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-      uid
-    })
-
-    setFormValue('');
-
-    dummy.current.scrollIntoView({ behavior: 'smooth' })
-
-  }
-
-  return (
-    <>
-      <main>
-        {messages && messages.map(message => <ChatMessage key={message.id} message={message} />)}
-
-        <div ref={dummy}></div>
-      </main>
-
-      <form onSubmit={sendMessage}>
-        <input value={formValue} onChange={(event) => setFormValue(event.target.value)} />
-        <button type="submit">SEND</button>
-      </form>
-    </>
-  )
-}
-
-function ChatMessage(props) {
-  const { text, uid } = props.message;
-
-  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
-
-  return (
-    <div className={`message ${messageClass}`} >
-      <p>{text}</p>
-    </div>
-  )
 }
 
 export default App;
